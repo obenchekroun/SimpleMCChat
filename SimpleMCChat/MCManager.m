8211797be2a033a425838c5049e98ba39bb8a18c
obenchekroun <othmane@me.com>
2015-08-18 19:27:51 +0200

    Added iPad compatibility +more unique MC service
    - Correction of all auto-layout (dynamic interface) making it
    compatible with iPad
    - Enabling Universal compiling
    - Changed name of MC service to something unique to this app

--- SimpleMCChat/MCManager.m
+++ SimpleMCChat/MCManager.m
@@ -94,12 +94,12 @@
 }
 
 -(void)setupMCBrowser{
-    _browser = [[MCBrowserViewController alloc] initWithServiceType:@"chat-files" session:_session];
+    _browser = [[MCBrowserViewController alloc] initWithServiceType:@"MCChatService" session:_session];
 }
 
 -(void)advertiseSelf:(BOOL)shouldAdvertise{
     if (shouldAdvertise) {
-        _advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:@"chat-files"
+        _advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:@"MCChatService"
                                                            discoveryInfo:nil
                                                                  session:_session];
         [_advertiser start];
