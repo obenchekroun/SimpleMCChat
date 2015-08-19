ff93d135ab68f20d45b8c169985e5f646fe0c3d6
obenchekroun <othmane@me.com>
2015-08-19 11:38:53 +0200

    Minor corrections of gpush

--- gpush.sh
+++ gpush.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 
-function notifyDone () { command="$@"; terminal-notifier -title "Terminal" -message "Done with '$command'!" -activate com.googlecode.iterm2; }
-function notifyError () { command="$@"; terminal-notifier -title "Terminal ERROR!" -message "'$command' exited with error!" -activate com.googlecode.iterm2; }
+function notifyDone () { command="$@"; terminal-notifier -title "Git update" -message "Done with '$command'!" -activate com.googlecode.iterm2; }
+function notifyError () { command="$@"; terminal-notifier -title "Git update ERROR!" -message "'$command' exited with error!" -activate com.googlecode.iterm2; }
 function wn () { ($@ && notifyDone $@) || notifyError $@; }
 function n () { $@; notifyDone $@;}
 
