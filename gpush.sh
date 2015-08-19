342116a64e4e5b22e72117bede345e3496b5d367
obenchekroun <othmane@me.com>
2015-08-19 11:21:05 +0200

    Minor corrections of README.md

--- gpush.sh
+++ gpush.sh
@@ -2,5 +2,5 @@
 git add --all
 git commit -m "$*"
 git push
-terminal-notifier -title "SimpleMCChat git update" -message "Task done. See terminal for potential errors" && badge
+terminal-notifier -title "SimpleMCChat git update" -message "Task done. See terminal for potential errors" && badges
 
