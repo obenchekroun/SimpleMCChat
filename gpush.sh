04f53a82dfff2c4d085b2fc3fccc1f724f6f71a9
obenchekroun <othmane@me.com>
2015-08-19 11:31:58 +0200

    Minor corrections of README.md

--- gpush.sh
+++ gpush.sh
@@ -1,4 +1,10 @@
 #!/usr/local/bin/bash
+
+function notifyDone () { command="$@"; terminal-notifier -title "Terminal" -message "Done with '$command'!" -activate com.googlecode.iterm2; }
+function notifyError () { command="$@"; terminal-notifier -title "Terminal ERROR!" -message "'$command' exited with error!" -activate com.googlecode.iterm2; }
+function wn () { ($@ && notifyDone $@) || notifyError $@; }
+function n () { $@; notifyDone $@;}
+
 git add --all
 git commit -m "$*"
 wn git push
