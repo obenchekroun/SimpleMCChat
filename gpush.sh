7ad767d1eabbffce2a085c47a85500683c666667
obenchekroun <othmane@me.com>
2015-08-19 11:15:05 +0200

    Minor corrections of README.md

--- gpush.sh
+++ gpush.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git add --all
 git commit -m "$*"
-git push
-terminal-notifier -title "SimpleMCChat pushed online" -message "Changes committed and pushed online"
+git push | terminal-notifier -title "SimpleMCChat pushed online"
 
